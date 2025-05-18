441ce135dbab605dfa65d4e192ae47bf0e385a88
omerbahar129 <omerbahar129@gmail.com>
2025-05-18 18:34:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def send_report():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
